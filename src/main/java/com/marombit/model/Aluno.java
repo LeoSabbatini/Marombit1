db5492f0d888617ae1664cb8346acbc19e246543
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 13:45:07 -0300

    Update Aluno.java

--- src/main/java/com/marombit/model/Aluno.java
+++ src/main/java/com/marombit/model/Aluno.java
@@ -1,12 +1,19 @@
 package com.marombit.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String cpf;
     private LocalDate bData;
     private Boolean mtcAtiva;
-    private String plano;
+    @Enumerated(EnumType.STRING)
+    private Plano plano;
 }
