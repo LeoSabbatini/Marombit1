20530605126dd1fce45e61d1605df5ba8006e5b5
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 13:48:33 -0300

    Update Aluno.java

--- src/main/java/com/marombit/model/Aluno.java
+++ src/main/java/com/marombit/model/Aluno.java
@@ -1,9 +1,15 @@
 package com.marombit.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class Aluno {
     @Id
