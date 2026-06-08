e1bb24882ad53fce2fbcc1dd90bcdaaa24191b44
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-08 13:35:56 -0300

    Initial commit

---
+++ src/main/java/com/marombit/model/Aluno.java
@@ -0,0 +1,12 @@
+package com.marombit.model;
+
+import java.time.LocalDate;
+
+public class Aluno {
+    private Long id;
+    private String name;
+    private String cpf;
+    private LocalDate bData;
+    private Boolean mtcAtiva;
+    private String plano;
+}
