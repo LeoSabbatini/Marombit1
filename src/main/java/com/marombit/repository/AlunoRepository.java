fbc748438797b9b808f6d242d85837c22fe174fa
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-09 09:07:03 -0300

    Update AlunoRepository.java

--- src/main/java/com/marombit/repository/AlunoRepository.java
+++ src/main/java/com/marombit/repository/AlunoRepository.java
@@ -4,6 +4,6 @@ import com.marombit.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
-
-
+    boolean existsByCpf(String cpf);
+    
 }
