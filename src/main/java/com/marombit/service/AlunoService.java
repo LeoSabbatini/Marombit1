a37852205d41a383ba526275e1f9f06ff962da79
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-09 10:40:37 -0300

    Update AlunoService.java

--- src/main/java/com/marombit/service/AlunoService.java
+++ src/main/java/com/marombit/service/AlunoService.java
@@ -6,13 +6,14 @@ import com.marombit.repository.AlunoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class AlunoService {
 
     @Autowired
