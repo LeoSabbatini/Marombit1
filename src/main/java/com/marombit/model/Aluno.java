7b05da856c09432b3dfd2553f5647ccfe591b56a
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-06-09 08:30:44 -0300

    Update Aluno.java

--- src/main/java/com/marombit/model/Aluno.java
+++ src/main/java/com/marombit/model/Aluno.java
@@ -1,6 +1,7 @@
 package com.marombit.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +16,21 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Por favor, insira um nome")
     private String name;
+
+    @NotBlank(message = "Por favor, insira um cpf")
     @Column(unique = true)
     private String cpf;
+
+    @NotBlank(message = "Por favor, insira sua data de nascimento")
     private LocalDate bData;
+
+    @NotBlank(message = "A matrícula ativa é obrigatória")
     private Boolean mtcAtiva;
+
+    @NotBlank(message = "Por favor, insira qual o plano")
     @Enumerated(EnumType.STRING)
     private Plano plano;
 }
